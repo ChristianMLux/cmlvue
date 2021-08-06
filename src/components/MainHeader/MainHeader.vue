90233e7f93f87bff86067a17441d2f22259f6dc5
ChristianMLux <christian.m.lux@gmail.com>
2021-08-06 19:30:41 +0200

    feat: redesigns the header area

---
+++ src/components/MainHeader/MainHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="main-header">
+    <div class="header-wrapper">
+      <h1 class="main-heading">{{ title }}</h1>
+      <p class="main-subheading">
+        {{ name }}
+      </p>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MainHeader",
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: "C.M.L",
+    },
+    name: {
+      type: String,
+      required: true,
+      default: "Christian M. Lux",
+    },
+  },
+};
+</script>
+
+<style scoped>
+.header-wrapper {
+  padding: 1rem;
+  display: flex;
+  flex-flow: row;
+  justify-content: space-between;
+}
+</style>
--- src/components/MainNav/MainNav.vue
+++ src/components/MainNav/MainNav.vue
@@ -40,7 +40,6 @@ ul {
   flex-flow: row;
   justify-content: space-evenly;
   list-style: none;
-  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
-    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+  box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
 }
 </style>
