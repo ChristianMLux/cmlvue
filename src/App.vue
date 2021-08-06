afb3249b86d288dd95bcf8fa0a534448431542aa
ChristianMLux <christian.m.lux@gmail.com>
2021-08-06 17:20:14 +0200

    feat: adds styling to mainNav

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <template>
   <MainNav />
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/profile">Profile</router-link> |
-    <router-link to="/showroom">Showroom</router-link> |
-    <router-link to="/cv">CV</router-link> |
-    <router-link to="/contact">Contact</router-link>
-  </div>
   <router-view />
 </template>
 
@@ -25,6 +18,21 @@ export default {
   box-sizing: border-box;
   margin: 0;
 }
+*::selection {
+  background-color: var(--primary-light);
+}
+body {
+  --primary-color: #ffb549;
+  --primary-light: #ffc062;
+  --success: #a3db33;
+  --error: #ed3221;
+  --warn: #ffc642;
+  --bright: #f2f2f3;
+  --grey: #c8cdd0;
+  --dark-grey: #415058;
+  --dark: #1f292e;
+  background-color: var(--bright);
+}
 
 #app {
   @import url("https://fonts.googleapis.com/css2?family=Open+Sans&display=swap");
@@ -33,19 +41,10 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: var(--dark);
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.router-link-active {
+  color: red;
 }
 </style>
--- src/components/MainNav/NavElement.vue
+++ src/components/MainNav/NavElement.vue
@@ -19,3 +19,23 @@ export default {
   },
 };
 </script>
+<style scoped>
+li {
+  padding: 0.5rem;
+}
+ul > li > a {
+  color: var(--dark);
+  text-decoration: none;
+  padding-left: 0.33rem;
+  border-left: 0.5px dotted var(--dark);
+}
+a {
+  font-weight: bold;
+}
+* .router-link-exact-active {
+  border-left: 4px dotted var(--primary-color);
+}
+a::first-letter .router-link-exact-active {
+  color: var(--primary-color);
+}
+</style>
