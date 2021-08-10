b3dcf87e6d8f38d00b0e7be3dd968f4365c76eb9
ChristianMLux <christian.m.lux@gmail.com>
2021-08-10 11:34:52 +0200

    feat: implements slider for old projects to showroom

--- src/views/Showroom.vue
+++ src/views/Showroom.vue
@@ -11,18 +11,69 @@
         could fill it with pictures. It was at this point that I had my first
         contact with Adobe Fireworks to create my first signature.
       </p>
+      <section>
+        <vueper-slides>
+          <vueper-slide
+            v-for="(slide, i) in slides"
+            :key="i"
+            :image="slide.image"
+            :title="slide.title"
+            :content="slide.content"
+          />
+        </vueper-slides>
+      </section>
     </content-area>
   </div>
 </template>
 
 <script>
+import { VueperSlides, VueperSlide } from "vueperslides";
+import "vueperslides/dist/vueperslides.css";
 export default {
   name: "Showroom",
   data() {
     return {
       contentHeading: "Showroom",
+      slides: [
+        {
+          title: "Slide #1",
+          content: "Slide content.",
+          image: "/assets/img/slides/slide01.jpg",
+        },
+        {
+          title: "Slide #2",
+          content: "Slide 2",
+          image: "/assets/img/slides/slide02.jpg",
+        },
+        {
+          title: "Slide #3",
+          content: "Slide 3",
+          image: "/assets/img/slides/slide03.jpg",
+        },
+        {
+          title: "Slide #4",
+          content: "Slide 4",
+          image: "/assets/img/slides/slide04.jpg",
+        },
+        {
+          title: "Slide #5",
+          content: "Slide 5",
+          image: "/assets/img/slides/slide05.jpg",
+        },
+        {
+          title: "Slide #6",
+          content: "Slide 6",
+          image: "/assets/img/slides/slide06.jpg",
+        },
+        {
+          title: "Slide #7",
+          content: "Slide 7",
+          image: "/assets/img/slides/slide07.jpg",
+        },
+      ],
     };
   },
+  components: { VueperSlides, VueperSlide },
 };
 </script>
 
